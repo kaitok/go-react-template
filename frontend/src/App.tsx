8600e7ee361e7dc5225d23299606dbcb2b176ae7
jo <kaitokinjo@gmail.com>
2023-04-15 22:33:44 +0900

    crate vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>test</div>
+      </div>
+    )
+  }
+}
+
+export default App
